fa1f75df88315385d50510861938dfacaaa79d54
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-20 20:08:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+  
+def sort_array_char_count(arr)
+  arr.sort {|left, right| left.length <=> right.length}
+end  
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  return arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.each do |letter|
+    letter[2] = "$"
+  end
+end
+def find_a(arr)
+  arr.select do |letter|
+    letter[0] == "a"
+  end
+end
+
+def sum_array(array_int)
+  array_int.inject(0, :+)
+end
+
+def add_s(array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word
+    else 
+      word + "s"
+    end
+  end
+end
+  
+  
+  
+  
+  
