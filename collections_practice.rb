03fad403c855ff04ff2e5edb37782946f50a8370
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-05-20 19:44:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+  
+def sort_array_char_count(arr)
+  arr.sort {|left, right| left.length <=> right.length}
+end  
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  return arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
